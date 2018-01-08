7a817a3b3e42e20bf40e414b934c89813c1b9c27
inti0294 <inti0294@192.168.1.89>
2018-01-08 12:26:04 +0100

    commit panier

--- eCommerce/src/fr/adaming/model/Panier.java
+++ eCommerce/src/fr/adaming/model/Panier.java
@@ -4,11 +4,19 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="paniers")
 public class Panier implements Serializable {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idPanier;
+	
 	//Traduction de l'association d'uml en java
 	@OneToMany(mappedBy="panier")
 	private List<LigneCommande> listeLigneCommande;
