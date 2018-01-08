f763948c477ca0ee866ce98bc6bf0413078145fa
marin <marin@192.168.1.196>
2018-01-08 12:03:40 +0100

    commit produit categorie persistence

--- eCommerce/src/fr/adaming/model/Produit.java
+++ eCommerce/src/fr/adaming/model/Produit.java
@@ -1,5 +1,140 @@
 package fr.adaming.model;
 
-public class Produit {
+import java.io.Serializable;
+import java.util.Arrays;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produits")
+public class Produit implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idProduit;
+	private String designation;
+	private String description;
+	private double prix;
+	private int quantite;
+	private boolean selectionne;
+	
+	@Lob
+	private byte[] photo;
+
+	// trasformation de l'associtation uml en java
+	@ManyToOne
+	@JoinColumn(referencedColumnName="idCategorie")
+	private Categorie categorie;
+
+	// constructeur par défaut
+	public Produit() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//constructeur sans id
+	public Produit(String designation, String description, double prix, int quantite, boolean selectionne,
+			byte[] photo) {
+		super();
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+
+	//COnstructeur avec id
+	public Produit(int idProduit, String designation, String description, double prix, int quantite,
+			boolean selectionne, byte[] photo) {
+		super();
+		this.idProduit = idProduit;
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+
+	// getters et setters
+	public int getIdProduit() {
+		return idProduit;
+	}
+
+	public void setIdProduit(int idProduit) {
+		this.idProduit = idProduit;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	// tostring
+	@Override
+	public String toString() {
+		return "Produit [idProduit=" + idProduit + ", designation=" + designation + ", description=" + description
+				+ ", prix=" + prix + ", quantite=" + quantite + ", selectionne=" + selectionne + ", photo="
+				+ Arrays.toString(photo) + "]";
+	}
 
 }
