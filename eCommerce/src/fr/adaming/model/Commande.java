e4744a332e406896fbd908fdfcf279cedb12e70c
inti0294 <inti0294@192.168.1.89>
2018-01-08 12:05:12 +0100

    commit client commande admin

---
+++ eCommerce/src/fr/adaming/model/Commande.java
@@ -0,0 +1,67 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="commandes")
+public class Commande implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCommande;
+
+	// Convertir la date java en format SQL
+	@Temporal(TemporalType.DATE) // L'objet de sortir de simpleDateFormt est
+									// dans le package util.Date, celui de
+	// la BD est de type sql.Date. C'est pourquoi on doit utiliser temporal
+	private Date dateCommande;
+
+	//Transformation des associations uml en java
+	@ManyToOne
+	private Client client;
+	
+	
+	public Commande() {
+		super();
+	}
+
+	public Commande(Date dateCommande) {
+		super();
+		this.dateCommande = dateCommande;
+	}
+
+	public Commande(Long idCommande, Date dateCommande) {
+		super();
+		this.idCommande = idCommande;
+		this.dateCommande = dateCommande;
+	}
+
+	public Long getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(Long idCommande) {
+		this.idCommande = idCommande;
+	}
+
+	public Date getDateCommande() {
+		return dateCommande;
+	}
+
+	public void setDateCommande(Date dateCommande) {
+		this.dateCommande = dateCommande;
+	}
+	
+	
+	
+
+}
